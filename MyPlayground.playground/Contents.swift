a62bc378622707f4a4bcbf17591770a6cf6bac0d
양시은 <mn07020@gmail.com>
2021-08-06 02:35:03 +0900

    스위프트 상속받은 연산 프로퍼티의 프로퍼티 감시자 구현

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+class Account{
+    var credit:Int = 0{
+        willSet{
+            print("잔액이 \(credit)원에서 \(newValue)원으로 변경될 예정입니다. ")
+        }
+        didSet{
+            print("잔액이 \(oldValue)원에서 \(credit)원으로 변경되었습니다. ")
+        }
+    }
+    
+    var dollarValue:Double{
+        get{
+            return Double(credit) / 1000.0
+        }
+        
+        set{
+            credit = Int(newValue * 1000)
+            print("잔액을 \(newValue)달러로 변경 중입니다. ")
+        }
+    }
+}
 
-var greeting = "Hello, playground"
+class ForeignAccount: Account{
+    override var dollarValue: Double{
+        willSet{
+            print("잔액이 \(dollarValue)달러에서 \(newValue)달러으로 변경될 예정입니다. ")
+        }
+        didSet{
+            print("잔액이 \(oldValue)달러에서 \(dollarValue)달러으로 변경되었습니다. ")
+        }
+    }
+}
+
+let myAccount: ForeignAccount = ForeignAccount()
+
+myAccount.credit = 1000
+
+myAccount.dollarValue = 2
