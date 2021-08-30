5ea42b3b6f5c38bb3348e14f71b6eeb84b1b2397
sieuns <mn07020@gmail.com>
2021-08-30 23:52:24 +0900

    스위프트 19장 타입캐스팅

--- MyPlayground9.playground/Contents.swift
+++ MyPlayground9.playground/Contents.swift
@@ -57,3 +57,87 @@ print(yourCoffee is Coffee)
 
 print(myCoffee is Latte)
 print(yourCoffee is Latte)
+
+//메타 타입
+protocol SomeProtocol { }
+class SomeClass: SomeProtocol { }
+
+let intType: Int.Type = Int.self
+let stringType: String.Type = String.self
+let classType: SomeClass.Type = SomeClass.self
+let protocolProtocol: SomeProtocol.Protocol = SomeProtocol.self
+
+var someType: Any.Type
+
+someType = intType
+print(someType)
+
+someType = stringType
+print(someType)
+
+someType = classType
+print(someType)
+
+someType = protocolProtocol
+print(someType)
+
+//type(of:) 함수와 .self의 사용
+print(type(of: coffee) == Coffee.self)
+print(type(of: coffee) == Americano.self)
+print(type(of: coffee) == Latte.self)
+
+print(type(of: coffee) == Americano.self)
+print(type(of: myCoffee) == Americano.self)
+print(type(of: yourCoffee) == Americano.self)
+
+print(type(of: coffee) == Latte.self)
+print(type(of: myCoffee) == Latte.self)
+print(type(of: yourCoffee) == Latte.self)
+
+//다운캐스팅
+if let actingOne: Americano = coffee as? Americano {
+    print("This is Americano")
+} else {
+    print(coffee.description)
+}
+
+if let actingOne: Latte = coffee as? Latte {
+    print("This is Latte")
+} else {
+    print(coffee.description)
+}
+
+if let actingOne: Coffee = coffee as? Coffee {
+    print("This is Just Coffee")
+} else {
+    print(coffee.description)
+}
+
+if let actingOne: Americano = myCoffee as? Americano {
+    print("This is Americano")
+} else {
+    print(coffee.description)
+}
+
+if let actingOne: Latte = myCoffee as? Latte {
+    print("This is Latte")
+} else {
+    print(coffee.description)
+}
+
+if let actingOne: Coffee = myCoffee as? Coffee {
+    print("This is Just Coffee")
+} else {
+    print(coffee.description)
+}
+
+let castedCoffee: Coffee = yourCoffee as! Coffee
+
+//런타임 오류! 강제 다운캐스팅 실패!
+//let castedAmericano: Americano = coffee as! Americano
+
+//항상 성공하는 다운캐스팅
+//let castedCoffee: Coffee = yourCoffee as Coffee
+
+
+
